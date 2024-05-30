f1ffcc47ebaeccbff75af16d729e8f5ea2d8f7d1
isurudilshan101 <isuru.dilshan101@gmail.com>
2024-05-30 12:32:04 +0530

    projects page updated

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
         "Effortlessly manage your cargo logistics with our responsive website, accessible from any device. Streamline operations and enhance efficiency with our user-friendly interface.",
       image: "/projects_img/cargoproject.png",
       technologies: ["React", "Tailwind Css"],
-      githubLink: "https://github.com/username/project1",
+      githubLink: "https://github.com/isurudilshan101/cargoweb",
       liveLink: "https://cargohub.netlify.app/",
     },
     {
@@ -17,22 +17,22 @@ const Projects = () => {
         "Efficiently manage tasks with ease through our intuitive interface, enabling seamless addition, deletion, and completion tracking for enhanced productivity.",
       image: "/projects_img/todoproject.png",
       technologies: ["React Js", "TypeScript", "Tailwind Css", "Ant Design"],
-      githubLink: "https://github.com/username/project2",
+      githubLink: "https://github.com/isurudilshan101/todo-app",
       liveLink: "https://easytasker.netlify.app/",
     },
     {
-      title: "Project 2",
+      title: "AI Tech Lab Web Site",
       description:
-        "Efficiently manage tasks with ease through our intuitive interface, enabling seamless addition, deletion, and completion tracking for enhanced productivity.",
-      image: "/projects_img/todoproject.png",
-      technologies: ["React Js", "TypeScript", "Tailwind Css"],
-      githubLink: "https://github.com/username/project2",
-      liveLink: "https://project2.com",
+        "Responsive website, accessible from any device. Streamline operations and enhance efficiency with our user-friendly interface.",
+      image: "/projects_img/ailabproject.png",
+      technologies: ["React Js", "Tailwind Css"],
+      githubLink: "https://github.com/isurudilshan101/react_web_site",
+      liveLink: "https://aitechlab.netlify.app/",
     },
   ];
 
   return (
-    <div className="container mx-auto   p-8 ">
+    <div className="container mx-auto p-8 ">
       <h1 className="text-4xl font-bold text-left mb-8">Projects</h1>
       <div>
         {projects.map((project, index) => (
