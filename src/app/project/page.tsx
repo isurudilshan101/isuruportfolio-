d8cb0d51d14a944eddf000fc77286be2e20cf1a3
isurudilshan101 <isuru.dilshan101@gmail.com>
2024-05-31 12:01:09 +0530

    projects page updated4

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -43,7 +43,7 @@ const Projects = () => {
       title: "Wheather App",
       description:
         "Welcome to our currency conversion app, where you can effortlessly convert your currencies with ease. Simply input the date, select your source and target currencies, and enter the amount in the source currency.Experience seamless currency conversions today!",
-      image: "/projects_img/phamacproject.png",
+      image: "/projects_img/weatherappproject.png",
       technologies: ["React Js", "API"],
       githubLink: "https://github.com/isurudilshan101/find_weather",
       liveLink: "https://climatetrackerapp.netlify.app/",
